45cd75834d3e163034722354a0d52740af096da0
Mohd Waqi Pervez <mohdwaqipervez@gmail.com>
2023-04-20 05:11:30 +0530

    explanation of each functionality

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pandas
+
+######################### Reading the phonetic alphabet from the csv file ################################
+
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_alphabet = {row.letter: row.code for (index, row) in data.iterrows()}
 
@@ -7,6 +10,8 @@ def generate_code():
     user = input("Enter a word: ").upper()
     try:
         phonetic_code = [phonetic_alphabet[letter] for letter in user]
+
+########################## Catching key error if user type other than alphabet #########################
 
     except KeyError:
         print("Type only alphabet")
